f48b1954a1306a819e7c433fb5598b39824a1c2f
Valery <vip.waleron@mail.ru>
2023-12-21 21:09:36 +0300

    job

--- keyboards.py
+++ keyboards.py
@@ -1,9 +1,25 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 menu = [
-    [InlineKeyboardButton(text="⏰ Текущее время", callback_data="getCurrentTime"),
-    InlineKeyboardButton(text="📝 Ввести время", callback_data="inputTime")]
+    [InlineKeyboardButton(text="⏰ Узнать текущее время", callback_data="getCurrentTime"),
+    InlineKeyboardButton(text="📝 Планировщик задач", callback_data="planner")]
 ]
+
+planner = [
+    [InlineKeyboardButton(text="✍ Запланировть новую задачу", callback_data="newPlan"),
+    InlineKeyboardButton(text="📖 Просмотреть список задач", callback_data="checkPlanner")],
+    [InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]
+]
+
+plans_list = [
+    [InlineKeyboardButton(text="✍ Удалить задачу", callback_data="removePlan"),
+    InlineKeyboardButton(text=" Очистить список дел", callback_data="clearPlanner")],
+    [InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]
+]
+
+plans_list_kb = InlineKeyboardMarkup(inline_keyboard=plans_list)
+planner_kb = InlineKeyboardMarkup(inline_keyboard=planner)
 menu = InlineKeyboardMarkup(inline_keyboard=menu)
-exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
-iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
+
+#exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
+#iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from aiogram import Bot, Dispatcher, types, F, Router
 from aiogram.filters import Command
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
-
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from aiogram.utils.formatting import as_marked_list
 
 import config
 import keyboards
@@ -18,39 +18,70 @@ dp = Dispatcher()
 scheduler = AsyncIOScheduler()
 
 
-date_choise = State() # Состояние, когда пользователь вводит дату
+class Planning(StatesGroup): # Планировщик (состояния)
+	planner = State() 
+	date_choise = State() # Состояние, когда пользователь вводит дату
+	setting_plan_name = State()
+	checking_plans = State()
 
 @router.message(Command("start"))
 async def echo_msg(message: types.Message):
 	await message.answer(f'Привет, {message.from_user.full_name}', reply_markup=keyboards.menu)
 
+# Главное меню
+@router.callback_query(F.data == "menu")
+async def openPlanner(query: types.CallbackQuery, state: FSMContext):
+	await bot.send_message(query.message.chat.id, 'Меню', reply_markup=keyboards.menu)
+	await state.clear()
+
+# Получить текущее время
 @router.callback_query(F.data == "getCurrentTime")
 async def writeTime(query: types.CallbackQuery):
 	structTime = time.localtime(time.time()) # Время типа struct_time(tm_year=2023 и т д)
 	answerTime = 'Время ' + str(structTime.tm_hour) + ':' + str(structTime.tm_min) + ', Дата ' + time.strftime("%d.%m.%Y") #%H:%M:%S  время, %d.%m.%Y дата
 	await bot.send_message(query.message.chat.id, answerTime)
 
+# Открыть меню планнера
+@router.callback_query(F.data == "planner")
+async def openPlanner(query: types.CallbackQuery, state: FSMContext):
+	await bot.send_message(query.message.chat.id, 'Планировщик задач', reply_markup=keyboards.planner_kb)
+	await state.set_state(Planning.planner)
 
-@router.callback_query(F.data == "inputTime")
-async def inputTime(query: types.CallbackQuery, state: FSMContext):
-	await bot.send_message(query.message.chat.id, 'Введите дату: (день.месяц.год часы:минуты)')
-	await state.set_state(date_choise)
+# Создать новую задачку
+@router.callback_query(Planning.planner, F.data == "newPlan")
+async def inputPlanName(query: types.CallbackQuery, state: FSMContext):
+	await bot.send_message(query.message.chat.id, 'Введите название события')
+	await state.set_state(Planning.setting_plan_name)
 
+# Обработать название события, записать в state data и запросить время
+@router.message(Planning.setting_plan_name, F.text)
+async def inputPlanTime(message: types.Message, state: FSMContext):
+	await state.update_data(planName = message.text)
+	await bot.send_message(message.chat.id, 'Введите дату (день.месяц.год часы:минуты), когда вам необходимо получить уведомление:')
+	await state.set_state(Planning.date_choise)
 
-async def sendNotification(chatid):
-	await bot.send_message(chatid, 'Внимание!')
+#Если от пользователя пришел не текст
+@router.message(Planning.setting_plan_name)
+async def inputPlanTime(message: types.Message, state: FSMContext):
+	await bot.send_message(message.chat.id, 'Упс! Попробуй ввести название события еще раз')
 
-@router.message(date_choise, F.text.regexp(r'\d{1,2}.\d{1,2}.\d{4}\s+\d{1,2}:\d{2}'))
+# Дата выбрана, создаем работу scheduler'у, если это возможно
+@router.message(Planning.date_choise, F.text.regexp(r'\d{1,2}.\d{1,2}.\d{4}\s+\d{1,2}:\d{2}'))
 async def timeGot(message: types.Message, state: FSMContext):
 	try:
 		gotTime = time.strptime(message.text, "%d.%m.%Y %H:%M") # получаем данные типа struct_time(tm_year=2023 и т д)
-		await bot.send_message(message.chat.id, f'Введенная дата в struct_time:\n{str(gotTime)}')
+		user_data = await state.get_data()
+		jobsCntr = len(scheduler.get_jobs())
+		jobid = f"job_{jobsCntr+1}_{message.chat.id}"
 		scheduler.add_job(
 			sendNotification,
 			"date",
 			run_date=datetime(gotTime.tm_year, gotTime.tm_mon, gotTime.tm_mday, gotTime.tm_hour, gotTime.tm_min),
-			args=(message.chat.id, )
+			args=(message.chat.id, user_data['planName'], ), #это кортеж touple
+			name=user_data['planName'],
+			id=jobid
 		)
+		await bot.send_message(message.chat.id, 'Событие успешно внесено в список дел!')
 	except ValueError:
 		# Если дата выходит за рамки (типа 40.13.2000 33:33)
 		await bot.send_message(message.chat.id, 'Введена некорректная дата!')
@@ -58,10 +89,32 @@ async def timeGot(message: types.Message, state: FSMContext):
 		await state.clear()
 		await bot.send_message(message.chat.id, 'Что дальше?', reply_markup=keyboards.menu)
 
-
-@router.message(date_choise)
+# Если регулярка не прошла проверку
+@router.message(Planning.date_choise)
 async def timeGotIncorrect(message: types.Message):
 	await bot.send_message(message.chat.id, 'Введенная дата некорректна, попробуйте еще раз')
+
+# Непосредственно функция отправки уведомления
+async def sendNotification(chatid, text):
+	await bot.send_message(chatid, f'Внимание! У вас сейчас {text}')
+
+# Составляем список задач и выводим его пользователю
+@router.callback_query(Planning.planner, F.data == "checkPlanner")
+async def checkPlans(query: types.CallbackQuery, state: FSMContext):
+	jobsList = scheduler.get_jobs()
+	if(len(jobsList) == 0):
+		await bot.send_message(query.message.chat.id, 'В планировщике пусто 👻👀', reply_markup=keyboards.planner_kb)
+		return
+	itemsForDisplay = as_marked_list(*list(map(lambda x: f"{x.name} ({x.next_run_time.strftime('%d %b %Y, %H:%M')})", jobsList)), marker="♦ ")
+	await bot.send_message(query.message.chat.id, **itemsForDisplay.as_kwargs(), reply_markup=keyboards.plans_list_kb)
+	await state.set_state(Planning.checking_plans)
+
+@router.callback_query(Planning.planner, F.data == "clearPlanner")
+async def clearPlanner(query: types.CallbackQuery, state: FSMContext):
+	jobsList = scheduler.remove_all_jobs()
+	await bot.send_message(query.message.chat.id, 'Успешно удалены все задачи', reply_markup=keyboards.menu)
+	await state.clear()
+
 
 @router.message()
 async def echoall(msg: types.Message):
